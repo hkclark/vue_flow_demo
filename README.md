db5581e58c13a5f4755e80246373b691c5def0c1
kclark <kclark@autoverify.net>
2025-11-05 10:07:32 -0500

    Add GH Actions

---
+++ README.md
@@ -0,0 +1,13 @@
+# Network Diagram Vue
+
+A simple demo of Vue Flow
+
+### Live Demo
+[https://hkclark.github.io/vue_flow_demo/]
+
+### Local Development
+
+```bash
+npm install
+npm run dev
+
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/vue_flow_demo/',
 })
 
